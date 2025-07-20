3efa3391f0fe97fdedfa6435afeceb32514dfd31
Tobyfox80 <olivertobywhite@outlook.com>
2025-07-20 21:50:28 +1200

    Add files via upload

---
+++ _arduino_programming/assessment_code/keypad_lock.ino
@@ -0,0 +1,163 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+int code[4] = {9,3,6,1};//code exists, 4 digits
+int currentdigit = 0;
+int correctdigits = 0;
+bool locked = true;
+bool settingcode = false;
+int input = 10;
+int previnput = 10;
+int digitsset = 0;
+void setup() {
+  Serial.begin(9600);
+  lcd.setCursor(0, 0);
+  lcd.print("Enter Code");
+  delay(1000);
+  lcd.clear();
+}
+void loop() {
+  int keyset1 = analogRead(A3);//read the resistor arrays
+  int keyset2 = analogRead(A2);
+  int keyset3 = analogRead(A1);
+  int keyset4 = analogRead(A0);
+  //keyset1 declaration. repeat for others, then build functions
+  if(keyset1 == 1023){
+      input = 3; 
+    }
+    else if(keyset1 >= 990 && keyset1 <= 1010){
+      input = 2;
+    } 
+    else if(keyset1 >= 505 && keyset1 <= 515){
+      input = 1; 
+    } 
+    else if(keyset2 == 1023){
+      input = 6; 
+    }
+    else if(keyset2 >= 990 && keyset2 <= 1010){
+      input = 5;
+    } 
+    else if(keyset2 >= 505 && keyset2 <= 515){
+      input = 4; 
+    } 
+    else if(keyset3 == 1023){
+      input = 9; 
+    }
+    else if(keyset3 >= 990 && keyset3 <= 1010){
+      input = 8;
+    } 
+    else if(keyset3 >= 505 && keyset3 <= 515){
+      input = 7; 
+    } 
+    else if(keyset4 == 1023){//check if code is correct or lock box when unlocked
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      if (currentdigit == 4 && correctdigits == 4){//if correct
+      locked = false;
+      lcd.print("Code correct!");
+      //move servo
+      delay(1000);
+      }
+      else if(locked == false){//if pressing when unlocked
+        locked = true;
+        lcd.print("Box locked!");
+        //move servo to locked
+        delay(1000);
+        lcd.clear();
+        lcd.print("Enter Code");
+        delay(1000);
+        lcd.clear();
+      }
+      else{//if not correct 
+      lcd.print("Wrong code!");
+      correctdigits = 0;
+      currentdigit = 0;
+      delay(1000);
+      lcd.clear();
+      lcd.print("Enter Code");
+      delay(1000);
+      lcd.clear();
+      }
+      input = 10;
+      previnput = 10;
+      correctdigits = 0;
+      currentdigit = 0;
+    }
+    else if(keyset4 >= 990 && keyset4 <= 1010){//setting a code
+      if(locked == false){
+          if(settingcode == false){
+            settingcode = true;
+            lcd.clear();
+            lcd.print("Set A Code!");
+            delay(1000);
+            lcd.clear();
+          }
+          else if(digitsset = 4){//if code is valid
+            settingcode = false;
+            lcd.clear();
+            lcd.print("Code Set!");
+            delay(1000);
+            lcd.clear();
+          }
+          else{//if invalid code
+            lcd.clear();
+            lcd.print("Invalid Code!");
+            delay(1000);
+            lcd.clear();
+          }
+      }
+      else{
+        lcd.clear();
+        lcd.print("Unlock First!");
+        delay(1000);
+        lcd.clear();
+        lcd.print("Enter Code");
+        delay(1000);
+        lcd.clear();
+      }
+      digitsset = 0;
+      currentdigit= 0;
+      delay(300);
+    } 
+    else if(keyset4 >= 505 && keyset4 <= 515){
+      input = 0; 
+    } 
+    else{
+      input = 10;
+    }
+  
+  if(currentdigit > 4){
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Wrong code!");
+    correctdigits = 0;
+    currentdigit = 0;
+    delay(500);
+    lcd.clear();
+    lcd.print("Enter Code");
+    delay(500);
+    lcd.clear();
+  }
+  if(input !=10){ 
+    if(settingcode == false){
+      if(input == code[currentdigit]){
+        correctdigits ++;
+      }
+    }
+    else if (settingcode == true){
+      if(digitsset <= 4){
+      code[currentdigit] = input;
+      digitsset ++;
+      }
+      else{
+        digitsset = 0;
+        currentdigit= 0;
+        lcd.clear();
+        lcd.print("Invalid Code!");
+        delay(1000);
+      }
+    }
+    lcd.print(input);
+    currentdigit ++;
+    delay(300);
+  }
+}
